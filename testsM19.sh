b11c8fe1a8ee70dbe28f4bdb0d70c3ebcbd15b4e
TChiclas <pedrombcustodio@gmail.com>
2019-04-11 20:51:23 +0100

    more stufffff

--- testsM19.sh
+++ testsM19.sh
@@ -15,6 +15,46 @@ prepare(){
 	
 	done
 }
+
+prepare_xml(){
+	cd  /home/tchiclas/Desktop/3ano/2semestre/Compiladores/m19 || exit
+	
+	for f in ../apoio_projeto/tests-201903052202/*.m19
+	do
+		echo ""
+		echo ""
+		echo "TEST: $f "
+		echo ""
+		echo ""
+		./m19 --tree $f
+		
+	
+	done
+}
+
+prepare_rmv(){
+	cd  /home/tchiclas/Desktop/3ano/2semestre/Compiladores/m19 || exit
+	rm /home/tchiclas/Desktop/3ano/2semestre/Compiladores/apoio_projeto/tests-201903052202/*.asm
+}
+
+
 #Opens m19 directory
 cd  /home/tchiclas/Desktop/3ano/2semestre/Compiladores/m19 || exit
-prepare
+
+
+argument="$1"
+
+case $argument in
+	-n)
+		prepare
+		;;
+	-xml)
+		prepare_xml
+		;;
+	-rm)
+		prepare_rmv
+		;;
+	*)
+		echo "Unknown argument"
+esac
+
