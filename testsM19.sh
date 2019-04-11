c570d274635548edb99d8b9371e9d72ca98ba455
TChiclas <pedrombcustodio@gmail.com>
2019-04-11 19:32:28 +0100

    fix testsM19.sh

--- testsM19.sh
+++ testsM19.sh
@@ -2,11 +2,14 @@
 
 prepare(){
 	cd  /home/tchiclas/Desktop/3ano/2semestre/Compiladores/m19 || exit
-	gnome-terminal
 	
 	for f in ../apoio_projeto/tests-201903052202/*.m19
 	do
-		echo "\n\nTEST: $f \n\n"
+		echo ""
+		echo ""
+		echo "TEST: $f "
+		echo ""
+		echo ""
 		./m19 $f
 		
 	
