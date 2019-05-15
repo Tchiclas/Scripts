c47beba451a2d75a2a9e1ba39b7b3ba441ade369
TChiclas <pedrombcustodio@gmail.com>
2019-05-15 16:20:14 +0100

    ready for the last thingy

--- testsM19.sh
+++ testsM19.sh
@@ -1,42 +1,54 @@
 #!/bin/bash
 
 prepare(){
-	cd  /home/tchiclas/Desktop/3ano/2semestre/Compiladores/m19 || exit
-	
-	for f in ../apoio_projeto/tests-201903052202/*.m19
+	cd  /home/tchiclas/Desktop/3ano/2semestre/Compiladores/apoio_projeto/tests-201903052202/ || exit
+
+	#./m19 --target asm A-01-1-N-ok.m19
+	for f in *.m19
 	do
 		echo ""
 		echo ""
-		echo "TEST: $f "
+		echo "run M19: $f "
 		echo ""
 		echo ""
-		./m19 $f
+		../../m19/m19 --target asm $f
+		
+	
+	done
+
+
+	#yasm -felf32 A-01-1-N-ok.asm
+	for f in *.asm
+	do
+		echo ""
+		echo ""
+		echo "Yasm: $f "
+		echo ""
+		echo ""
+		yasm -felf32 $f
+		
+	
+	done
+
+	#ld -melf_i386 -o main A-01-1-N-ok.o -L${HOME}/compiladores/root/usr/lib -lrts
+	for f in *.o
+	do
+		echo ""
+		echo ""
+		echo "Running through the 90s: $f "
+		echo ""
+		echo ""
+		ld -melf_i386 -o main $f -L${HOME}/compiladores/root/usr/lib -lrts
+		./main
 		
 	
 	done
 }
 
-prepare_xml(){
-	cd  /home/tchiclas/Desktop/3ano/2semestre/Compiladores/m19 || exit
-	
-	for f in ../apoio_projeto/tests-201903052202/*.m19
-	do
-		echo ""
-		echo ""
-		echo "TEST: $f "
-		echo ""
-		echo ""
-		./m19 --tree $f
-		
-	
-	done
-}
-#java -cp xml2dot.xml2dot bla.xml
-#dot -Tsvg bla.dot
 prepare_rmv(){
-	cd  /home/tchiclas/Desktop/3ano/2semestre/Compiladores/m19 || exit
-	rm /home/tchiclas/Desktop/3ano/2semestre/Compiladores/apoio_projeto/tests-201903052202/*.xml
 	rm /home/tchiclas/Desktop/3ano/2semestre/Compiladores/apoio_projeto/tests-201903052202/*.asm
+	rm /home/tchiclas/Desktop/3ano/2semestre/Compiladores/apoio_projeto/tests-201903052202/*.o
+	rm /home/tchiclas/Desktop/3ano/2semestre/Compiladores/apoio_projeto/tests-201903052202/main
 }
 
 
@@ -50,9 +62,6 @@ case $argument in
 	-n)
 		prepare
 		;;
-	-xml)
-		prepare_xml
-		;;
 	-rm)
 		prepare_rmv
 		;;
@@ -60,3 +69,4 @@ case $argument in
 		echo "Unknown argument"
 esac
 
+	#../../m19/m19 --target asm A-01-1-N-ok.m19 ;  yasm -felf32 A-01-1-N-ok.asm ;ld -melf_i386 -o main A-01-1-N-ok.o -L${HOME}/compiladores/root/usr/lib -lrts
